eb5c5f02d4588646662c6092872f6975be1c713e
David Ramirez <davidraviles@gmail.com>
2023-10-05 14:27:05 -0700

    updates(block-content): modified button alignment on mobile and added option to animate headers.

--- components/block/block-content/index.vue
+++ components/block/block-content/index.vue
@@ -1,12 +1,14 @@
 <template lang='pug' src='./index.pug'></template>
 
 <script>
+import { titleAnimation } from '~/resources/mixins'
 import BlockButton from '~/components/block/block-button'
 
 export default {
   components: {
     BlockButton
   },
+  mixins: [titleAnimation],
   props: {
     props: {
       type: Object,
@@ -23,6 +25,37 @@ export default {
     forceCenter: {
       type: Boolean,
       default: false
+    },
+    animate: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted () {
+    if (this.$store.state.siteIsLoaded) {
+      this.handleAnimation()
+    } else {
+      this.$store.watch(
+        state => this.$store.state.siteIsLoaded,
+        (newVal) => {
+          if (newVal) {
+            this.handleAnimation()
+          }
+        }
+      )
+    }
+  },
+  methods: {
+    handleAnimation () {
+      this.$nextTick(() => {
+        console.log('call')
+        if ((this.props.hero_header && this.props.header && this.animate) || (this.props.header && this.animate)) {
+          this.$_titleAnimation(this.$refs.header, '0', '24', 1, 0.5)
+        }
+        if (this.props.subheader && this.animate) {
+          this.$_titleAnimation(this.$refs.subheader, '0', '24', 1, 0.5)
+        }
+      })
     }
   }
 }
