f93b4da4c24a7815bb58734db3b57ff5c47d2033
Dave <qooqu@users.noreply.github.com>
2024-02-16 14:39:40 -0800

    adds zsiqchat script

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,23 @@ export default {
   async asyncData () {
     const props = await setData('home')
     return { props }
+  },
+  head () {
+    return {
+      script: [
+        {
+          hid: 'zsiqchat_script',
+          id: 'zsiqchat',
+          type: 'text/javascript',
+          innerHTML: `
+            var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "siq9ab237bd619d0dd24fd56dcdb444214727ebcc8e49edd2d17dbdc029bbf301d7", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zohopublic.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
+          `
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        zsiqchat_script: ['innerHTML']
+      }
+    }
   }
 }
 </script>
