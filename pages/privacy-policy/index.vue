c4cc9bda2e2b06ef58e777ea6b7867f68ef33b4b
sam-twdy <stweedy.st@gmail.com>
2024-10-23 14:34:56 -0400

    adds 10DLC compliant privacy policy/opt-in #84899

---
+++ pages/privacy-policy/index.vue
@@ -0,0 +1,26 @@
+<template lang='pug' src='./index.pug'></template>
+
+<script>
+import { setData, setMeta } from '~/resources/utils'
+import PageSections from '~/components/page-sections'
+
+export default {
+  transition: 'fade',
+  components: {
+    PageSections
+  },
+  async asyncData () {
+    const props = await setData('privacy-policy')
+    return { props }
+  },
+  head () {
+    return setMeta(this.props)
+  }
+}
+</script>
+
+<style>
+.pages-privacy-policy {
+  padding-top: 6rem;
+}
+</style>
